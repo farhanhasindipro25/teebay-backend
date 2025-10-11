a65a5618ce92d2b369f72cbb208c4e21f5866f09
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2025-10-12 00:16:28 +0600

    chore: Added env and docker setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const PORT: number = Number(configService.get('PORT'));
+
+  await app.listen(PORT).then(() => {
+    console.log(`
+       ████████████████████████████████████████████████████████████████████
+       █                                                                  █
+       █                 🌐  WELCOME TO TEEBAY SERVER  🌐                 █
+       █                                                                  █
+       ████████████████████████████████████████████████████████████████████
+       
+       ──────────────────────────────────────────────────────────────────
+                              SERVER INFORMATION                         
+       ──────────────────────────────────────────────────────────────────
+       
+                 🔥  Status       : Running
+                 🛠️   Environment  : ${configService.get('TARGET_ENV')}
+                 🌐  Port         : ${configService.get('PORT')}
+                 ⏰  Startup Time : ${new Date().toLocaleString()}
+       
+       ──────────────────────────────────────────────────────────────────       
+       `);
+  });
 }
 bootstrap();
